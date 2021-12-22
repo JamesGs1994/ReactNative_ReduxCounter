a7afc9207d97c95c9e24a53e6a2ba53b7cf30db6
James Gs <81745865+JamesGs1994@users.noreply.github.com>
2021-12-22 10:53:53 +0530

    ReactNative_ReduxCounter

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import Counter from './Components/Counter';
+import {Provider} from 'react-redux';
+import store from './Redux/store';
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    );
+  }
+}
